71d279c12ade0ea78f157e3a5419ac4d8b8081f3
Cory Boudreaux <cboud31@gmail.com>
2024-04-03 21:14:00 -0500

    logic for multiple operations, clear button

--- calc.js
+++ calc.js
@@ -1,32 +1,17 @@
-console.log("Hello from calc.js!");
-
-// What variables do we need?
-/*
-[x] input1: First # entered into calculator
-[x] input2: Second # entered into calculator (after operand sign)
-[x] operator: operator sign passed into operate()
-[X] mathArr: [input1, input2, operator] array passed into operate()
-[X] result: output of operate(); used to update display
-[] resultArr: stores previous results, use resultArr.reduce() to keep tally
-*/
 let input1 = null;
 let input2 = null;
 let operator = "+";
-const mathArray = [];
+let mathArray = [];
 let result = 0;
 
 const display = document.querySelector(".display-main");
 const numberBtns = document.querySelectorAll(".number");
 const operatorBtns = document.querySelectorAll(".operate");
 const equalsBtn = document.querySelector(".equals");
-// console.log(numberBtns);
+const clearBtn = document.querySelector(".clear");
+const deletBtn = document.querySelector(".delete");
+
 display.textContent = result;
-/*
-[X] Functions for 4 math operations: add, subtract, multiply, divide
-[X] Variables for operate() inputs
-[X] Functions -> operate()
-[] Update display when pressing numberBtns
-*/
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
@@ -46,9 +31,15 @@ const operate = ([num1, operator, num2]) => {
   }
 };
 
+const clearDisplay = () => {
+  result = 0;
+  input1 = null;
+  input2 = null;
+  display.textContent = result;
+};
+
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    // [X] Limit the display to 9 characters
     if (display.textContent.length === 9) {
       return;
     }
@@ -73,14 +64,9 @@ numberBtns.forEach((btn) => {
 
 operatorBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    // What needs to happen when we hit this button?
-    // [x] Store the display value (input1) in the mathArray
-    // [x] Store the operator button value in the mathArray
     operator = btn.value;
     mathArray.push(parseInt(input1), operator);
-
     console.log(mathArray);
-    console.dir(display);
     // [] Highlight style of pressed button
   });
 });
@@ -94,5 +80,16 @@ equalsBtn.addEventListener("click", () => {
     console.log(result);
     display.textContent = result;
     // return;
+    input1 = result;
+    input2 = null;
+    mathArray = [];
   }
+});
+
+clearBtn.addEventListener("click", () => {
+  clearDisplay();
+});
+
+deletBtn.addEventListener("click", () => {
+  console.log("clicked deletBtn!");
 });
